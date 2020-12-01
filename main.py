8b42bbaf528495019d99f59b6d936ed7cf955eb3
powerferren@gmail.com <powerferren@gmail.com>
2020-12-01 12:24:33 +0200

    Deleting 'Thumbs.db' from the list

--- main.py
+++ main.py
@@ -3,4 +3,13 @@ import os
 
 if __name__ == "__main__":
     directory = r'D:\ОПИСИ\описи Ф\Ф 114 оп 1'
-    print(os.listdir(directory))
+    list_of_files = os.listdir(directory)
+    print(list_of_files)
+    length_of_list = len(list_of_files)
+    print(length_of_list)
+    if list_of_files[0] == 'Thumbs.db':
+        list_of_files.pop(0)
+        print(list_of_files)
+    elif list_of_files[length_of_list - 1] == 'Thumbs.db':
+        list_of_files.pop()
+        print(list_of_files)
