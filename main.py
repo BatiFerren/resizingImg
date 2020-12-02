99900de7b7f1b9813d1b7ced09d52db0c6659922
powerferren@gmail.com <powerferren@gmail.com>
2020-12-02 13:58:18 +0200

    Created renaming all files in the directory. Resizing will be executed after second running of script

--- main.py
+++ main.py
@@ -4,17 +4,22 @@ import os
 
 if __name__ == "__main__":
     # TODO: Have to processing russian sumbols in the path of files
-    directory = r'D:\OPISI\opisiF\114_1'
+    directory = r'D:/OPISI/opisiF/F112_2'
+    # print(directory.encode('utf8').decode('cp1251'))
     list_of_files = os.listdir(directory)
     length_of_list = len(list_of_files)
     if list_of_files[0] == 'Thumbs.db':
-        os.remove(directory + '\\' + list_of_files[0])
+        os.remove(directory + '/' + list_of_files[0])
         list_of_files.pop(0)
     elif list_of_files[length_of_list - 1] == 'Thumbs.db':
-        os.remove(directory + '\\' + list_of_files[length_of_list - 1])
+        os.remove(directory + '/' + list_of_files[length_of_list - 1])
         list_of_files.pop()
+    i=0
     for item in list_of_files:
-        img_path = directory + '\\' + item
+        os.rename(directory + '/' + item, directory + '/' + str(i) + '.jpg')
+        i+=1
+    for item in list_of_files:
+        img_path = directory + '/' + item
         img = cv2.imread(img_path)
         old_width = int(img.shape[1])
         old_height = int(img.shape[0])
